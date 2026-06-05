9637b4df7195c42b58259c30072d33913558554f
Manasvi_Kailash_Pusam <manasvipusam@gmail.com>
2026-06-05 10:47:32 +0530

    Implement smallest divisor using binary search
    Fix syntax errors and implement binary search for smallest divisor.

---
+++ BinarySearch/SmallestDivisorOptimalMethod.java
@@ -0,0 +1,43 @@
+ import java.util.Scanner;
+ public class Main
+ {
+     public static int smallestDivisor(int[]a,int threshold)
+     {
+         int=a.length
+         for(int i=0;i<n;i++)
+         {
+             max=Math.ma(max,num);
+         }
+         int low=1;
+         int high=max;
+         int ans=-1;
+         while(low<=high)
+         {
+             int mid=low+(high-low)/2;
+              int sum=CalulateSum(a,divisor);
+              if(sum<=threshold)
+              {
+                  return divisor;
+                  high=mid-1;
+              }
+              else
+              {
+                  low=mid+1;
+              }
+         }
+         return ans;
+         
+         
+         
+     }
+ 
+     public static int CalculateSum(int[] a,int diviosr)
+     for(int i=0;i<a.length;i++)
+     {
+         int sum=0;
+         sum=sum+(int)Math.ceil(double)num/2));
+           
+     }
+     return sum;
+     
+ }
