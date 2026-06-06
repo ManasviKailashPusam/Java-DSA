4f0eaf56bb57ed90094b623bbca5ce0891a3557b
Manasvi_Kailash_Pusam <manasvipusam@gmail.com>
2026-06-06 11:42:02 +0530

    Implement addLast method to add node at end

---
+++ LinkedList/AddLast.java
@@ -0,0 +1,16 @@
+//add last
+      public satic void addLast(int data)
+      {
+          Node newNode=new Node(data);
+          if(head==null)
+          {
+              head=newNode;
+              return;
+          }
+          Node temp=head;
+          while(temp.next!=null)
+          {
+              temp=temp.next;
+          }
+          temp.next=newNode;
+      }
