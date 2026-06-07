8d80d6cdab196cecb3e492dbfcbe5340ee39226b
Manasvi_Kailash_Pusam <manasvipusam@gmail.com>
2026-06-07 13:42:52 +0530

    Refactor findMiddleElement method
    Refactored findMiddleElement method to use a brute force approach for finding the middle element of a linked list.

--- LinkedList/findMiddleElementBruteMethod.java
+++ LinkedList/findMiddleElementBruteMethod.java
@@ -1,14 +1,25 @@
-public static void main(String[] args)
+ public static void main(String[]args)
 {
-   public static int findMiddleElement(Node head)
-    {
-       Node slow=head;
-       Node fast=head;
-      while(fast!=null   &&  fast.next!=null)
-      {
-           slow=slow.next;
-           fast=fast.next.next;
-      }
-    return slow.data;
-    }
- }
+            public static int findMiddleElement(Node head)
+        { 
+            Node head=head;
+            int count=0;
+            while(temp!=null)
+            {
+             count++;
+             temp=temp.next;
+             }
+             int midNode=(midNode/2)+1;
+    
+             while(temp!=null)
+             {
+               midNode--;
+               if(midNode==0)
+              {
+                 break;
+                temp=temp.next;
+               }
+           }
+               return temp.data;
+   }
+
