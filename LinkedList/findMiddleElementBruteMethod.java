f2dbf8464c2e097ab6d63e0239661adc41f99c3f
Manasvi_Kailash_Pusam <manasvipusam@gmail.com>
2026-06-07 13:41:56 +0530

    Implement findMiddleElement method in LinkedList

---
+++ LinkedList/findMiddleElementBruteMethod.java
@@ -0,0 +1,14 @@
+public static void main(String[] args)
+{
+   public static int findMiddleElement(Node head)
+    {
+       Node slow=head;
+       Node fast=head;
+      while(fast!=null   &&  fast.next!=null)
+      {
+           slow=slow.next;
+           fast=fast.next.next;
+      }
+    return slow.data;
+    }
+ }
