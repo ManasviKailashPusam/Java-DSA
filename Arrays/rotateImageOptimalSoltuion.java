c20fa75df839d4dabdfbbedd47c3715bfdd99d30
Manasvi_Kailash_Pusam <manasvipusam@gmail.com>
2026-07-07 16:07:20 +0530

    Add optimal solution for rotating image
    Implement optimal solution for rotating an image represented by a 2D matrix.

---
+++ Arrays/rotateImageOptimalSoltuion.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ public class Main
+ {
+     public static int rotateImage(int[][]matrix)
+     {
+         int n=matrix.length;
+          for(int i=0;i<n-1;i++)
+          {
+              for(int j=0;j<n;j++)
+              {
+                 int temp=mat[i][j];
+                 mat[i][j]=mat[j][i];
+                 mat[j][i]=temp;
+              }
+          }
+          for(int i=0;i<n;i++)
+          {
+              int left=0;
+              int right=n-1;
+              while(left<right)
+              {
+                  int temp=mat[i][left];
+                  mat[i][left]=mat[i][right];
+                  mat[i][right]=temp;
+                  left++;
+                  right--;
+              }
+          }
+     }
+ }
